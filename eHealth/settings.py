82881af7d4446cf67bdb5ccd3b59c03628cf7669
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-03-06 13:26:05 +0100

    update settings.py for heroku deployment

--- eHealth/settings.py
+++ eHealth/settings.py
@@ -1,6 +1,6 @@
 import os
 
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,4 +141,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
